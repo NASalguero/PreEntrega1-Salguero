06d23c12175fc17ddf6eba6aa438fb868b55ca0a
Nicolás Salguero <nico_crs@hotmail.com>
2023-12-11 01:27:09 -0300

    img error

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ class App extends React.Component {
         <div className="MerchCard">
           <ItemListContainer nombre="212 VIP Black" diseñador="Carolina Herrera" precio="87000" imagen={imagen1}/>
           <ItemListContainer nombre="Bad Boy Le Parfum" diseñador="Carolina Herrera" precio="79000" imagen="./assets/bad-boy-leparfum.png"/>
-          <ItemListContainer nombre="Eros" diseñador="Versace" precio="82000" imagen="./assets/eros-versace.png"/>
+          <ItemListContainer nombre="Eros" diseñador="Versace" precio="82000" imagen="https://www.google.com/url?sa=i&url=https%3A%2F%2Ffourthsense.co%2Fversace-eros-edt-review%2F&psig=AOvVaw1FMrKpQnfiibbZXbUL8y16&ust=1702354924745000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIjE_LbEhoMDFQAAAAAdAAAAABAD"/>
           <ItemListContainer nombre="Le Male" diseñador="Jean Paul Gaultier" precio="85000" imagen="./assets/le-male.png"/>
         </div>
 
