0711752c779eec614e9bf85e200f3366119219a9
Nicolás Salguero <nico_crs@hotmail.com>
2023-12-11 01:28:24 -0300

    precio

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ import Footer from "./components/Footer/Footer";
 
 const imagen1 = "./assets/212vip-black-CH.webp";
 
-
-
-
 class App extends React.Component {
   render() {
     return (
@@ -17,13 +14,33 @@ class App extends React.Component {
         <Header title="Fallen Angels" subtitle="Perfumes" />
 
         <div className="MerchCard">
-          <ItemListContainer nombre="212 VIP Black" diseñador="Carolina Herrera" precio="87000" imagen={imagen1}/>
-          <ItemListContainer nombre="Bad Boy Le Parfum" diseñador="Carolina Herrera" precio="79000" imagen="./assets/bad-boy-leparfum.png"/>
-          <ItemListContainer nombre="Eros" diseñador="Versace" precio="82000" imagen="https://www.google.com/url?sa=i&url=https%3A%2F%2Ffourthsense.co%2Fversace-eros-edt-review%2F&psig=AOvVaw1FMrKpQnfiibbZXbUL8y16&ust=1702354924745000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIjE_LbEhoMDFQAAAAAdAAAAABAD"/>
-          <ItemListContainer nombre="Le Male" diseñador="Jean Paul Gaultier" precio="85000" imagen="./assets/le-male.png"/>
+          <ItemListContainer
+            nombre="212 VIP Black"
+            diseñador="Carolina Herrera"
+            precio="$87000"
+            imagen={imagen1}
+          />
+          <ItemListContainer
+            nombre="Bad Boy Le Parfum"
+            diseñador="Carolina Herrera"
+            precio="$79000"
+            imagen="./assets/bad-boy-leparfum.png"
+          />
+          <ItemListContainer
+            nombre="Eros"
+            diseñador="Versace"
+            precio="$82000"
+            imagen="https://www.google.com/url?sa=i&url=https%3A%2F%2Ffourthsense.co%2Fversace-eros-edt-review%2F&psig=AOvVaw1FMrKpQnfiibbZXbUL8y16&ust=1702354924745000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIjE_LbEhoMDFQAAAAAdAAAAABAD"
+          />
+          <ItemListContainer
+            nombre="Le Male"
+            diseñador="Jean Paul Gaultier"
+            precio="$85000"
+            imagen="./assets/le-male.png"
+          />
         </div>
 
-        <Footer/>
+        <Footer />
       </div>
     );
   }
